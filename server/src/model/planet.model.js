c4548c07bf188139be05ab33d4a2c03a8a40b502
Hasan-aga <careerforma@gmail.com>
2022-09-22 19:20:55 +0300

    add planets data and filter function

--- server/src/model/planet.model.js
+++ server/src/model/planet.model.js
@@ -1,3 +1,35 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const { createReadStream } = require("fs");
 
-module.exports = planets;
+const maybeHabitable = function (planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+const habitablePlanets = [];
+
+createReadStream("./kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (chunk) => {
+    if (maybeHabitable(chunk)) habitablePlanets.push(chunk);
+  })
+  .on("end", () => {
+    console.log(`${habitablePlanets.length} habitable planets found.`);
+    console.log(
+      habitablePlanets.map(
+        (planet, index) => index + " " + planet["kepler_name"]
+      )
+    );
+  })
+  .on("error", (error) => console.error(`we have a problem, ${error}`));
+
+module.exports = habitablePlanets;
