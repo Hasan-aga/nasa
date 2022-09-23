a0004c7e413e2c4fae6ff4a198211b9d9c4e7842
Hasan-aga <careerforma@gmail.com>
2022-09-23 20:24:03 +0300

    add data validation for launch

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,27 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddLaunch(req, res) {
   const launch = req.body;
+
+  // validation
+  if (
+    !launch.mission ||
+    !launch.launchDate ||
+    !launch.rocket ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Some required props are missing!",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+
+  if (launch.launchDate.toString().toLowerCase() === "invalid date") {
+    return res.status(400).json({
+      error: "Invalid date!",
+    });
+  }
+
   addLaunch(launch);
   return res.status(201).json(launch);
 }
