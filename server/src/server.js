388d7842f888e54a545780794d5d9aca372a977b
Hasan-aga <careerforma@gmail.com>
2022-09-27 21:47:19 +0300

    add mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 const http = require("http");
 const { app } = require("./app");
 const { planetPromise } = require("./model/planet.model");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-
 const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection is ready.");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(error);
+});
 
 async function startServerAfterLoadingData() {
   try {
+    await mongoose.connect(process.env.MONGO_URL);
     await planetPromise;
     server.listen(PORT, () => console.log("Listening on port ", PORT));
   } catch (error) {
